124ce3e5c2886d83aaa029a7365fb9eb603b5163
Philipp Buehler <pbuehler@sysfive.com>
2018-11-30 23:21:41 +0100

    parse into some config defaults

--- builder/openbsd-vmm/builder.go
+++ builder/openbsd-vmm/builder.go
@@ -2,6 +2,8 @@ package openbsdvmm
 
 import (
 	"fmt"
+	"os/exec"
+	"path/filepath"
 
 	"github.com/hashicorp/packer/common"
 	"github.com/hashicorp/packer/helper/communicator"
@@ -30,10 +32,42 @@ func (b *Builder) Prepare(raws ...interface{}) ([]string, error) {
 
 	var errs *packer.MultiError
 	errs = packer.MultiErrorAppend(errs, b.config.Comm.Prepare(&b.config.ctx)...)
-
-	if b.config.ImageName == "" {
-		errs = packer.MultiErrorAppend(errs, errors.New("Missing image name"))
+	if b.config.VMName == "" {
+		b.config.VMName = "packer-" + b.config.PackerBuildName
 	}
+	if b.config.SourceImage == "" {
+		errs = packer.MultiErrorAppend(errs, errors.New("Missing source_image name"))
+	}
+	if b.config.ImageName == "" {
+		b.config.ImageName = "image-" + b.config.PackerBuildName
+	}
+	if b.config.DiskSize == "" {
+		b.config.DiskSize = "5G"
+	}
+	switch b.config.DiskFormat {
+	case "raw,qcow2":
+		// valid, use as is
+	case "":
+		b.config.DiskFormat = "raw"
+	default:
+		errs = packer.MultiErrorAppend(errs, errors.New("Unsupported disk_format name: "+b.config.DiskFormat))
+	}
+	if b.config.RAMSize == "" {
+		b.config.RAMSize = "512M"
+	}
+	if b.config.Inet4 == "" {
+		b.config.Inet4 = "dhcp" //XXX: some syntax check isIP4+prefix
+	}
+	if b.config.Inet4GW == "" {
+		// "is ip4"?
+	}
+	if b.config.Inet6 == "" {
+		b.config.Inet4 = "autoconf" //XXX: some syntax check isIP6+prefix
+	}
+	if b.config.Inet6GW == "" {
+		// "is ip6"?
+	}
+	// XXX: DNS
 
 	if len(errs.Errors) > 0 {
 		return nil, errors.New(errs.Error())
