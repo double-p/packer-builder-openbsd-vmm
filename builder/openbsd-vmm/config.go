a79485ee85b169b97720a1e0f81874f58eab130f
Philipp Buehler <pbuehler@sysfive.com>
2018-11-30 22:38:18 +0100

    config values likely needed

--- builder/openbsd-vmm/config.go
+++ builder/openbsd-vmm/config.go
@@ -8,12 +8,26 @@ import (
 
 type Config struct {
 	common.PackerConfig `mapstructure:",squash"`
+	common.HTTPConfig   `mapstructure:",squash"`
+	common.ISOConfig    `mapstructure:",squash"`
 	Comm                communicator.Config `mapstructure:",squash"`
 
-	ImageName   string `mapstructure:"image_name"`
-	SourceImage string `mapstructure:"source_image"`
-
-	UserData   string `mapstructure:"user_data"`
+	VMName      string `mapstructure:"vm_name"`
+	Console     bool   `mapstructure:"console"` // attach a console (to debug)
+	SourceImage string `mapstructure:"source_image"` //installNN.fs
+	ImageName   string `mapstructure:"image_name"`  // artifact from build
+	DiskSize    string `mapstructure:"disk_size"` // as vmctl -s
+	DiskFormat  string `mapstructure:"disk_format"` // as vmctl create
+	RAMSize     string `mapstructure:"ram_size"` // as vmctl -m
+        // not everybody lives in autoconf/DHCP; populate for hostname.vi0
+	Inet4       string `mapstructure:"inet4"` // hostname.if 'inet'
+	Inet4GW     string `mapstructure:"inet4gw"` // mygate 'inet'
+	Inet6       string `mapstructure:"inet6"` // hostname.if 'inet6'
+	Inet6GW     string `mapstructure:"inet6gw"` // mygate 'inet6'
+        DNS         string `mapstructure:"nameservers"` // resolv.conf, comma-separated
+ 
+	OutDir      string `mapstructure:"output_directory"`
+	UserData    string `mapstructure:"user_data"`
 
 	ctx interpolate.Context
 }
